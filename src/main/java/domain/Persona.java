b925415f0e162aeadbb88a81843e922e431c3ae7
hector morales <hector.morales@logicalsoft.co>
2021-12-10 17:08:43 -0500

    primer commit

---
+++ src/main/java/domain/Persona.java
@@ -0,0 +1,66 @@
+package domain;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Persona {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    
+    public Persona(){}
+    
+    public Persona(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", telefono=" + telefono + '}';
+    }
+    
+}
+
